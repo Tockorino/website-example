edf135196ecc15b490da9d0909e4223cf2d1419f
Théo <la217960@student.helha.be>
2023-10-03 14:50:46 +0200

    test final

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coucou les amis
+          coucou c'est jimmy
         </p>
         <a
           className="App-link"
